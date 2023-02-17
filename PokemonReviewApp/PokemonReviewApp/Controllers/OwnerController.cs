5b6d6782245530386a70828c15989064ad071847
Mark Harmon <neothej1sec@gmail.com>
2023-02-17 12:35:22 -0600

    PokemonAPI
    Added My First API whhich is a pokemon api using .NET core and entity framework

---
+++ PokemonReviewApp/PokemonReviewApp/Controllers/OwnerController.cs
@@ -0,0 +1,125 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : Controller
+    {
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly ICountryRepository _countryRepository;
+        private readonly IMapper _mapper;
+
+        public OwnerController(IOwnerRepository ownerRepository, ICountryRepository countryRepository, IMapper mapper)
+        {
+            this._ownerRepository = ownerRepository;
+            this._mapper = mapper;
+            this._countryRepository= countryRepository;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Owner>))]
+        public IActionResult GetOwners()
+        {
+            var owners = _mapper.Map<List<OwnerDto>>(_ownerRepository.GetOwners());
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(owners);
+        }
+        [HttpGet("{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult Getowner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+            var owner = _mapper.Map<OwnerDto>(_ownerRepository.GetOwner(ownerId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(owner);
+        }
+        [HttpGet("{ownerId}/pokemon")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+            var poke = _mapper.Map<List<PokemonDto>>(_ownerRepository.GetPokemonByOwner(ownerId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(poke);
+        }
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateOwner([FromQuery] int countryId, [FromBody] OwnerDto ownerCreate)
+        {
+            if (ownerCreate == null)
+                return BadRequest();
+            var owner = _ownerRepository.GetOwners()
+                .Where(c => c.LastName.Trim().ToUpper() == ownerCreate.LastName
+                .TrimEnd().ToUpper()).FirstOrDefault();
+            if (owner != null)
+            {
+                ModelState.AddModelError("", "Owner already exists");
+                return StatusCode(422, ModelState);
+            }
+            var ownerMap = _mapper.Map<Owner>(ownerCreate);
+            ownerMap.Country = _countryRepository.GetCountry(countryId);
+            if (!_ownerRepository.CreateOwner(ownerMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+            return Ok(owner);
+        }
+        [HttpPut("{ownerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateOwner(int ownerId, [FromBody] OwnerDto updatedOwner)
+        {
+            if (updatedOwner == null)
+                return BadRequest(ModelState);
+            if (ownerId != updatedOwner.Id)
+                return BadRequest(ModelState);
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var ownerMap = _mapper.Map<Owner>(updatedOwner);
+            if (!_ownerRepository.UpdateOwner(ownerMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating owner");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+        [HttpDelete("{ownerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+            var ownerToDelete = _ownerRepository.GetOwner(ownerId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            if (!_ownerRepository.DeleteOwner(ownerToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong deleting owner");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+    }
+}
+    
---
+++ PokemonReviewApp/PokemonReviewApp/Controllers/ReviewController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : Controller
+    {
+        private readonly IReviewRepository _reviewRespository;
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IReviewerRepository _reviewerRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewController(IReviewRepository reviewRespository,
+            IReviewerRepository reviewerRepository, 
+            IPokemonRepository pokemonRepository, 
+            IMapper mapper)
+        {
+            _reviewRespository = reviewRespository;
+            _pokemonRepository = pokemonRepository;
+            _reviewerRepository = reviewerRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        public IActionResult GetReviews()
+        {
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewRespository.GetReviews());
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(reviews);
+        }
+        [HttpGet("{reviewId}")]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRespository.ReviewExists(reviewId))
+                return NotFound();
+            var review = _mapper.Map<ReviewDto>(_reviewRespository.GetReview(reviewId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(review);
+        }
+        [HttpGet("pokemon/{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewsForAPokemon(int pokeId)
+        {
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewRespository.GetReviewsOfAPokemon(pokeId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(reviews);
+        }
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateReview([FromQuery] int reviewerId, [FromQuery] int pokeId, [FromBody] ReviewDto reviewCreate)
+        {
+            if (reviewCreate == null)
+                return BadRequest();
+            var reviews = _reviewRespository.GetReviews()
+                .Where(c => c.Title.Trim().ToUpper() == reviewCreate.Title
+                .TrimEnd().ToUpper()).FirstOrDefault();
+            if (reviews != null)
+            {
+                ModelState.AddModelError("", "Review already exists");
+                return StatusCode(422, ModelState);
+            }
+            var reviewMap = _mapper.Map<Review>(reviewCreate);
+            reviewMap.Pokemon = _pokemonRepository.GetPokemon(pokeId);
+            reviewMap.Reviewer = _reviewerRepository.GetReviewer(reviewerId);
+            if (!_reviewRespository.CreateReview(reviewMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+            return Ok("Successfully posted");
+        }
+        [HttpPut("{reviewId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateReview(int reviewId, [FromBody] ReviewDto updatedReview)
+        {
+            if (updatedReview == null)
+                return BadRequest(ModelState);
+            if (reviewId != updatedReview.Id)
+                return BadRequest(ModelState);
+            if (!_reviewRespository.ReviewExists(reviewId))
+                return NotFound();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var reviewMap = _mapper.Map<Review>(updatedReview);
+            if (!_reviewRespository.UpdateReview(reviewMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating review");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+        [HttpDelete("{reviewId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteReview(int reviewId)
+        {
+            if (!_reviewRespository.ReviewExists(reviewId))
+                return NotFound();
+            var reviewToDelete = _reviewRespository.GetReview(reviewId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            if (!_reviewRespository.DeleteReview(reviewToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong deleting review");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+    }
+}

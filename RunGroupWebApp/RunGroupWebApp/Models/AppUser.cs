a84046f568ec15e9faafc19106e5285bd7c6df66
Mark Harmon <neothej1sec@gmail.com>
2023-02-20 13:23:13 -0600

    db migration err

--- RunGroupWebApp/RunGroupWebApp/Models/AppUser.cs
+++ RunGroupWebApp/RunGroupWebApp/Models/AppUser.cs
@@ -5,6 +5,8 @@ namespace RunGroupWebApp.Models
 {
     public class AppUser
     {
+        [Key] 
+        public string Id { get; set; }
         public int? Pace { get; set; }
         public int? Mileage { get; set; }
         public Address? Address { get; set; }

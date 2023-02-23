1e7a743e92c9a564b4963f15f4e0b3a3fffcb358
Mark Harmon <neothej1sec@gmail.com>
2023-02-23 12:57:55 -0600

    Implemented Dependancy Injection

--- RunGroopWebApp/RunGroopWebApp/Program.cs
+++ RunGroopWebApp/RunGroopWebApp/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Repository;
 using RunGroupWebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IClubRepository, ClubRepository>();
+builder.Services.AddScoped<IRaceRepository, RaceRepository>();
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
